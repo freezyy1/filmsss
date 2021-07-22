41fb9cd8feb705a59868817a05d7930b0a4a6754
Dmitrii <80770507+freezyy1@users.noreply.github.com>
2021-07-22 16:09:03 +0300

    first commit

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,41 @@
+const Footer = () => {
+    return (
+        <div>
+            <footer className="page-footer indigo darken-1">
+                <div className="container">
+                    <div className="row">
+                        <div className="col l6 s12">
+                            <h5 className="white-text">My Websites on pure css</h5>
+                            <ul>
+                                <li><a className="grey-text text-lighten-3" href="https://freezyy1.github.io/">Site of company</a></li>
+                                <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                                <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+
+                            </ul>
+
+
+
+                        </div>
+                        <div className="col l4 offset-l2 s12">
+                            <h5 className="white-text">Projects on react</h5>
+                            <ul>
+                                <li><a className="grey-text text-lighten-3" href="#!">Movies search</a></li>
+                                <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                                <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer-copyright">
+                    <div className="container">
+                        © {new Date().getFullYear()} Copyright
+                        <a className="grey-text text-lighten-4 right" href="https://github.com/freezyy1">My github</a>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export {Footer}
---
+++ src/layout/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+    return <nav className="indigo darken-1">
+        <div className="nav-wrapper">
+        <div href="#" className="brand-logo">Movies search</div>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="!#">Repo</a></li>
+        </ul>
+        </div>
+    </nav>
+}
+
+export {Header}
